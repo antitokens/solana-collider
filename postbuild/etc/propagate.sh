f3f657d3a5696d27fe78e6a1e5f4c7cc0d36e3bb
sshmatrix <cryptosauron@protonmail.com>
2025-02-27 20:14:52 +0530

    $FILE

---
+++ postbuild/etc/propagate.sh
@@ -0,0 +1,10 @@
+FILE=$1
+
+# Check if on main
+if git rev-parse --abbrev-ref HEAD | grep -q 'main'; then
+    echo "✓ On main"
+    git add $FILE && git commit -S -m '$FILE' && git push && git checkout backup && git checkout main -- $FILE && git commit -S -m '$FILE' && git push && git checkout devnet && git checkout main -- $FILE && git commit -S -m '$FILE' && git push && git checkout localnet && git checkout main -- $FILE && git commit -S -m '$FILE' && git push && git checkout prod && git checkout main -- $FILE && git commit -S -m '$FILE' && git push
+else
+    echo "✕ Not on main"
+fi
+
