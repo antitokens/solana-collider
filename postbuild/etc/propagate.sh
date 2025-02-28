684e56affb222b1e89517a757b0a636c499dd387
sshmatrix <cryptosauron@protonmail.com>
2025-02-28 16:13:37 +0530

    Propagate postbuild/etc/propagate.sh

--- postbuild/etc/propagate.sh
+++ postbuild/etc/propagate.sh
@@ -1,9 +1,32 @@
+#!/bin/bash
+
 FILES=$1
+
+# Print the files to confirm input
+echo "Files to propagate: $FILES"
 
 # Check if on main
 if git rev-parse --abbrev-ref HEAD | grep -q 'main'; then
     echo "✓ On main"
-    git add $FILES && git commit -S -m '$FILES' && git push && git checkout backup && git checkout main -- $FILES && git commit -S -m '$FILES' && git push && git checkout devnet && git checkout main -- $FILES && git commit -S -m '$FILES' && git push && git checkout localnet && git checkout main -- $FILES && git commit -S -m '$FILES' && git push && git checkout prod && git checkout main -- $FILES && git commit -S -m '$FILES' && git push && git checkout main
+
+    # Define an array of branches to propagate changes
+    branches=("backup" "devnet" "localnet" "prod")
+
+    # Commit and push changes on main
+    git add $FILES &&
+        git commit -S -m "Propagate $FILES" &&
+        git push
+
+    # Iterate through each branch and propagate changes
+    for branch in "${branches[@]}"; do
+        git checkout $branch &&
+            git checkout main -- $FILES &&
+            git commit -S -m "Propagate $FILES" &&
+            git push
+    done
+
+    # Return to main
+    git checkout main
 else
     echo "✕ Not on main"
 fi
